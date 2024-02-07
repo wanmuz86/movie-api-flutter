3a9e885d5db0ef3b68e21c3081b23b3db4576188
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-07 10:29:43 +0800

    add step 4

--- lib/models/movie_search.dart
+++ lib/models/movie_search.dart
@@ -1,7 +1,7 @@
 // 1) Define the class
 
 class MovieSearch {
-  //2 ) Define the properties
+  //2 ) Define the properties (camelCase)
   final String title;
   final String year;
   final String imdbId;
@@ -9,5 +9,14 @@ class MovieSearch {
   final String poster;
 
   //3) Create the constructor
-MovieSearch({required this.title, required this.year, required this.imdbId, required this.type, required this.poster});
+MovieSearch({required this.title, required this.year, required this.imdbId,
+  required this.type, required this.poster});
+
+// 4) Create json to object/class transformer
+// The right part needs to follow Json format
+  // The left part follows our property
+factory MovieSearch.fromJson(dynamic json){
+  return MovieSearch(title: json["Title"], year: json["Year"],
+      imdbId: json["imdbID"], type: json["Type"], poster: json["Poster"]);
+}
 }
