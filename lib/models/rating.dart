7c3ca45574e17a7339d5dc2d142f9ae1d7145420
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-07 12:24:22 +0800

    add rating class

---
+++ lib/models/rating.dart
@@ -0,0 +1,21 @@
+class Rating {
+  final String source;
+  final String value;
+
+  Rating({required this.source, required this.value});
+
+  factory Rating.fromJson(dynamic json){
+    return Rating(source: json["Source"], value: json["Value"]);
+  }
+  static List<Rating> ratingsFromJson(dynamic json ){
+    List<Rating> results = List.empty(growable: true);
+    if (json != null){
+      json.forEach((v)=>{
+        results.add(Rating.fromJson(v))
+      });
+      return results;
+    }
+    return results;
+  }
+  
+}
