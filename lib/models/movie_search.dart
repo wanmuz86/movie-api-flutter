1c220e5dd106c1956206ecc35e8471f2d448315a
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-07 10:35:55 +0800

    add step 5

--- lib/models/movie_search.dart
+++ lib/models/movie_search.dart
@@ -19,4 +19,24 @@ factory MovieSearch.fromJson(dynamic json){
   return MovieSearch(title: json["Title"], year: json["Year"],
       imdbId: json["imdbID"], type: json["Type"], poster: json["Poster"]);
 }
+
+//5) Array of JSON to List of Class/Object transformer
+// If you are working with an array you need step 5
+// If you are working with an object .. no need step 5
+
+  static List<MovieSearch> moviesFromJson(dynamic json ){
+    var searchResult = json["Search"]; // Refer to the "Search" in the json
+    List<MovieSearch> results = List.empty(growable: true); // Create an empty growable List
+
+    if (searchResult != null){
+
+      searchResult.forEach((v)=>{ // Go through the Array in Json
+        results.add(MovieSearch.fromJson(v)) // Add it to the list
+      });
+      return results;
+    }
+    return results;
+  }
+
+
 }
