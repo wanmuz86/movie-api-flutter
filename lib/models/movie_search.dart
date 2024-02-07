7617d48d2f3354cc8d7f376af63f9a0b00906ae8
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-07 10:20:44 +0800

    create MovieSearch class

---
+++ lib/models/movie_search.dart
@@ -0,0 +1,13 @@
+// 1) Define the class
+
+class MovieSearch {
+  //2 ) Define the properties
+  final String title;
+  final String year;
+  final String imdbId;
+  final String type;
+  final String poster;
+
+  //3) Create the constructor
+MovieSearch({required this.title, required this.year, required this.imdbId, required this.type, required this.poster});
+}
